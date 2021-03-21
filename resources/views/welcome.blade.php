b505c1967b32550ae42714b35835d5e542fc9ff6
Aggerholm1337 <92aggerholm@gmail.com>
2021-03-21 19:44:58 +0100

    Started testing with css.. Not yet working.!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="assets/css/style.css" rel="stylesheet">
         <title>Laravel</title>
-
-        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-
     </head>
     <body class="antialiased">
         <p align="center" class="test">Laravel up and running!</p>
