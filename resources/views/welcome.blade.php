041cd47bd995618c050f6ed5abee1acb46297600
Aggerholm1337 <92aggerholm@gmail.com>
2021-03-21 20:06:09 +0100

    Started testing with css.. Not yet working.!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
         <title>Laravel</title>
     </head>
     <body class="antialiased">
