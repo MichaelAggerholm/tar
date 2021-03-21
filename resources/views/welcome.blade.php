396c92764f315b07e35792946297a1020887a591
Aggerholm1337 <92aggerholm@gmail.com>
2021-03-21 20:30:34 +0100

    crap styles still not working on server.. !

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-{{--        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">--}}
         <title>Laravel</title>
-        <!-- Styles -->
-        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+        @include('partials.head')
+
     </head>
     <body class="antialiased">
         <p align="center" class="test">Laravel up and running!</p>
