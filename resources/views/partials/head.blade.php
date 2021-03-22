8bafd27899004bea2c9a22c1b50ca2c1f182623c
mbt <mbt@tigermedia.dk>
2021-03-22 09:39:16 +0100

    fixed include

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,4 +7,4 @@
 
 <!-- Styles -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<link rel="stylesheet" type="text/less" href="{{ url('/assets/css/style.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ url('/assets/css/style.css') }}">
